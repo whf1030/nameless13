b67d79d3a9899d2c5047d18d86753c96acb195f6
whf1030 <wanghaofei@163.com>
2026-05-03 20:01:16 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,52 @@
+wget --no-check-certificate -O tgbot.sh https://raw.githubusercontent.com/nameless3721/nash/master/tgbot.sh && chmod +x tgbot.sh && ./tgbot.sh
+
+vi $Path/Telegram-chatbot/config.json
+
+wget --no-check-certificate -O sc.sh https://raw.githubusercontent.com/nameless3721/nash/master/sc.sh && chmod +x sc.sh && ./sc.sh
+
+```
+wget --no-check-certificate -O rustss.sh https://raw.githubusercontent.com/nameless3721/nash/master/rustss.sh
+chmod +x rustss.sh
+./rustss.sh
+systemctl restart rustss
+systemctl status rustss
+
+wget --no-check-certificate -O uninstall-rustss.sh https://raw.githubusercontent.com/nameless3721/nash/master/uninstall-rustss.sh
+chmod +x uninstall-rustss.sh
+./uninstall-rustss.sh
+```
+```
+wget https://github.com/pymumu/smartdns/releases/download/Release38.1/smartdns.1.2022.10.22-1309.x86_64-debian-all.deb
+dpkg -i smartdns*.deb
+wget https://raw.githubusercontent.com/nameless3721/nash/master/smartdns.conf
+mv -f smartdns.conf /etc/smartdns/
+mkdir -p /etc/systemd/resolved.conf.d/
+cat >/etc/systemd/resolved.conf.d/99-dns.conf << EOF
+[Resolve]
+DNS=127.0.0.1 1.1.1.1
+EOF
+ln -s -f /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
+mkdir -p /etc/systemd/resolved.conf.d/
+cat >/etc/systemd/resolved.conf.d/98-disable-127-53.conf << EOF
+[Resolve]
+DNSStubListener=no
+EOF
+systemctl daemon-reload && systemctl restart systemd-resolved.service && systemctl status -l systemd-resolved.service --no-pager
+cat /etc/resolv.conf
+
+#替换解锁ip
+sed -i "s/#server 88.88.88.88 -group nf/server 解锁ip -group nf/g" /etc/smartdns/smartdns.conf
+rm -rf smartdns*.deb
+systemctl restart smartdns
+systemctl enable smartdns --now
+dpkg -r smartdns
+apt-mark hold smartdns
+#apt-mark unhold smartdns
+nano /etc/ssh/sshd_config
+systemctl restart sshd
+```
+```
+#解锁nftv
+wget -qO nfset.sh https://raw.githubusercontent.com/nameless3721/nash/master/nfset.sh && bash nfset.sh
+```
---
+++ rustss.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+CONF="/etc/rustss/rustss-server.json"
+SYSTEMD="/etc/systemd/system/rustss.service"
+apt-get -y install nano wget
+cd ~/
+wget --no-check-certificate -O rustss.tar.xz https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.12.0-alpha.9/shadowsocks-v1.12.0-alpha.9.x86_64-unknown-linux-musl.tar.xz
+tar -xvJf rustss.tar.xz
+rm -f rustss.tar.xz
+chmod +x ssserver
+chmod +x sslocal
+chmod +x ssmanager
+chmod +x ssurl
+mv -f ssserver /usr/local/bin/
+mv -f sslocal /usr/local/bin/
+mv -f ssmanager /usr/local/bin/
+mv -f ssurl /usr/local/bin/
+if [ -f ${CONF} ]; then
+  echo "Found existing config..."
+  else
+  if [ -z ${PSK} ]; then
+    PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
+    echo "Using generated PSK: ${PSK}"
+  else
+    echo "Using predefined PSK: ${PSK}"
+  fi
+  read -p "输入端口或者23456" port
+  [ -z "${port}" ] && port='23456'
+  read -p "输入密码或者随机" rustsspwd
+  [ -z "${rustsspwd}" ] && rustsspwd=${PSK}
+  mkdir /etc/rustss/
+  echo "Generating new config..."
+  echo "{" >>${CONF}
+  echo "    \"server\": \"0.0.0.0\"," >>${CONF}
+  echo "    \"server_port\": $port," >>${CONF}
+  echo "    \"password\": \"$rustsspwd\"," >>${CONF}
+  echo "    \"timeout\": 60," >>${CONF}
+  echo "    \"mode\":\"tcp_and_udp\"," >>${CONF}
+  echo "    \"method\": \"aes-128-gcm\"" >>${CONF}
+  echo "}" >>${CONF}
+fi
+if [ -f ${SYSTEMD} ]; then
+  echo "Found existing service..."
+  systemctl daemon-reload
+  systemctl restart rustss
+else
+  echo "Generating new service..."
+  echo "[Unit]" >>${SYSTEMD}
+  echo "Description=Rustss Proxy Service" >>${SYSTEMD}
+  echo "After=network.target" >>${SYSTEMD}
+  echo "" >>${SYSTEMD}
+  echo "[Service]" >>${SYSTEMD}
+  echo "Type=simple" >>${SYSTEMD}
+  echo "LimitAS=infinity" >>${SYSTEMD}
+  echo "LimitRSS=infinity" >>${SYSTEMD}
+  echo "LimitCORE=infinity" >>${SYSTEMD}
+  echo "LimitNOFILE=65535" >>${SYSTEMD}
+  echo "ExecStart=/usr/local/bin/ssserver -c /etc/rustss/rustss-server.json -v" >>${SYSTEMD}
+  echo "StandardOutput=file:/etc/rustss/rust.log" >>${SYSTEMD}
+  echo "StandardError=file:/etc/rustss/rust.log" >>${SYSTEMD}
+  echo "Restart=always" >>${SYSTEMD}
+  echo "RestartSec=10" >>${SYSTEMD}
+  echo "" >>${SYSTEMD}
+  echo "[Install]" >>${SYSTEMD}
+  echo "WantedBy=multi-user.target" >>${SYSTEMD}
+  systemctl daemon-reload
+  systemctl enable rustss
+  systemctl start rustss
+  ssurl -e /etc/rustss/rustss-server.json
+fi
+echo '##########################################################'
+echo '#修改配置:nano /etc/rustss/rustss-server.json'
+echo '#启动ss:systemctl restart rustss'
+echo '#获取url自行改ip:ssurl -e /etc/rustss/rustss-server.json'
+echo '##########################################################'
